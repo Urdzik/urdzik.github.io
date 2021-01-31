002d0c6b28f274b3a65017d2eb2d827c0a144518
Slavik Urdzik <urdziksv@gmail.com>
2021-01-31 14:53:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$('.advantages__slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: `<button type="button" class="slick-prev"><</button>`,
+    nextArrow: `<button type="button" class="slick-next">></button>`
+});
+
+$('.catalogue__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: `<button type="button" class="slick-prev"><</button>`,
+    nextArrow: `<button type="button" class="slick-next">></button>`
+});
+
